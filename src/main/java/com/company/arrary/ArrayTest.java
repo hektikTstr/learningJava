6e39b583144b16e8c5a13f277d99316928bef07e
shawn.zhang <shawn.zhang@ringcentral.com>
2016-12-29 23:32:36 +0800

    add remove method

--- src/main/java/com/company/arrary/ArrayTest.java
+++ src/main/java/com/company/arrary/ArrayTest.java
@@ -2,13 +2,16 @@ package com.company.arrary;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        Scoreboard board = new Scoreboard(3);
+        Scoreboard board = new Scoreboard(4);
         board.add(new GameEntry("shawn", 98));
         board.add(new GameEntry("andy", 85));
         board.add(new GameEntry("ian", 100));
         board.add(new GameEntry("janice", 84));
         board.add(new GameEntry("nasen", 85));
         board.add(new GameEntry("seven", 88));
+        board.remove(1);
         board.add(new GameEntry("sherry", 99));
+        board.remove(2);
+        board.remove(0);
     }
 }
--- src/main/java/com/company/arrary/Scoreboard.java
+++ src/main/java/com/company/arrary/Scoreboard.java
@@ -4,22 +4,52 @@ public class Scoreboard {
     private int numEntries = 0;
     private GameEntry[] board;
     public Scoreboard(int capacity) {
+        if (capacity <= 0) {
+            throw new UnsupportedOperationException("capacity must be a positive integer");
+        }
         board = new GameEntry[capacity];
     }
     public void add(GameEntry e) {
         int newScore = e.getScore();
-        int index = numEntries;
-        if (index < board.length || newScore > board[index - 1].getScore()) {
-            if (index < board.length) {
-                board[numEntries++] = e;
+        if (numEntries < board.length || newScore > board[numEntries - 1].getScore()) {
+            if (numEntries < board.length) {
+                numEntries++;
             }
+            int index = numEntries - 1;
             while (index > 0 && newScore > board[index - 1].getScore()) {
-                if (index < board.length) {
-                    board[index] = board[index - 1];
-                }
-                board[index - 1] = e;
+                board[index] = board[index - 1];
                 index--;
             }
+            board[index] = e;
         }
+        System.out.println("#############################");
+        System.out.println("-> add: " + e);
+        printBoard();
+    }
+
+    private void printBoard() {
+        for (GameEntry g : board) {
+            if (g != null) {
+                System.out.println(g);
+            }
+        }
+    }
+
+    public GameEntry remove(int i) {
+        int index = i;
+        if (index < 0 || index >= board.length) {
+            throw new IndexOutOfBoundsException("index should be in range");
+        }
+        GameEntry temp = board[index];
+        while (index < numEntries - 1) {
+            board[index] = board[index + 1];
+            index++;
+        }
+        board[index] = null;
+        numEntries--;
+        System.out.println("#############################");
+        System.out.println("-> remove: " + i);
+        printBoard();
+        return temp;
     }
 }
