e33f2da3f6732bf0426dbff8a343c94e2f05750f
shawn.zhang <shawn.zhang@ringcentral.com>
2016-12-30 08:14:43 +0800

    add insertion sort.

--- src/main/java/com/company/arrary/ArrayTest.java
+++ src/main/java/com/company/arrary/ArrayTest.java
@@ -1,17 +1,37 @@
 package com.company.arrary;
 
+import java.util.Arrays;
+
 public class ArrayTest {
     public static void main(String[] args) {
-        Scoreboard board = new Scoreboard(4);
-        board.add(new GameEntry("shawn", 98));
-        board.add(new GameEntry("andy", 85));
-        board.add(new GameEntry("ian", 100));
-        board.add(new GameEntry("janice", 84));
-        board.add(new GameEntry("nasen", 85));
-        board.add(new GameEntry("seven", 88));
-        board.remove(1);
-        board.add(new GameEntry("sherry", 99));
-        board.remove(2);
-        board.remove(0);
+//        Scoreboard board = new Scoreboard(4);
+//        board.add(new GameEntry("shawn", 98));
+//        board.add(new GameEntry("andy", 85));
+//        board.add(new GameEntry("ian", 100));
+//        board.add(new GameEntry("janice", 84));
+//        board.add(new GameEntry("nasen", 85));
+//        board.add(new GameEntry("seven", 88));
+//        board.remove(1);
+//        board.add(new GameEntry("sherry", 99));
+//        board.remove(2);
+//        board.remove(0);
+
+        char[] chars = new char[] {'f', 'b', 'c', 'a', 'd', 'e'};
+        insertionSort(chars);
+        System.out.println(Arrays.toString(chars));
+    }
+    private static void insertionSort(char[] chars) {
+        if (chars.length <= 1) {
+            return;
+        }
+        for (int x = 1; x < chars.length; x++) {
+            int y = x;
+            while (y > 0 && chars[y] < chars[y - 1]) {
+                char temp = chars[y - 1];
+                chars[y - 1] = chars[y];
+                chars[y] = temp;
+                y--;
+            }
+        }
     }
 }
