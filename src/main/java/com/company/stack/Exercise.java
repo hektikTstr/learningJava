9ab9b5cc3e115d4f0c23fcdf4d6acd47be4e28d9
hektikTstr <shawn.zhang.xmn@gmail.com>
2017-02-04 08:44:31 +0800

    chapter 6.3 exercise.

--- src/main/java/com/company/stack/Exercise.java
+++ src/main/java/com/company/stack/Exercise.java
@@ -1,5 +1,9 @@
 package com.company.stack;
 
+import com.company.queue.Deque;
+import com.company.queue.LinkedDeque;
+import com.company.queue.Queue;
+
 public class Exercise {
     //R-6.4
     public static <E> void transfer(Stack<E> orig, Stack<E> target) {
@@ -20,8 +24,84 @@ public class Exercise {
         }
     }
 
+    //R-6.10
+    public class StackAdapter<E> implements Stack<E> {
+        private Deque<E> deque = null;
+
+        public StackAdapter() {
+            deque = new LinkedDeque<>();
+        }
+
+        public StackAdapter(Deque<E> deque) {
+            this.deque = deque;
+        }
+
+        @Override
+        public int size() {
+            return deque.size();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return deque.isEmpty();
+        }
+
+        @Override
+        public void push(E e) {
+            deque.addFirst(e);
+        }
+
+        @Override
+        public E top() {
+            return deque.first();
+        }
+
+        @Override
+        public E pop() {
+            return deque.removeFirst();
+        }
+    }
+
+    //R-6.11
+    public class QueueAdapter<E> implements Queue<E> {
+        private Deque<E> deque = null;
+
+        public QueueAdapter() {
+            deque = new LinkedDeque<>();
+        }
+
+        public QueueAdapter(Deque<E> deque) {
+            this.deque = deque;
+        }
+
+        @Override
+        public int size() {
+            return deque.size();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return deque.isEmpty();
+        }
+
+        @Override
+        public void enqueue(E e) {
+            deque.addLast(e);
+        }
+
+        @Override
+        public E first() {
+            return deque.first();
+        }
+
+        @Override
+        public E dequeue() {
+            return deque.removeFirst();
+        }
+    }
+
     public static void main(String[] args) {
-        Stack<Integer> orig = new ArrayStack<>();
+        Stack<Integer> orig = new Exercise().new StackAdapter<>();
         Stack<Integer> target = new ArrayStack<>();
         orig.push(5);
         orig.push(3);
