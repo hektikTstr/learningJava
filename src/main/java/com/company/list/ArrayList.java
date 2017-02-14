84a56c22df169d83089b2590c6dcf622cea01b52
hektikTstr <shawn.zhang.xmn@gmail.com>
2017-02-14 08:06:11 +0800

    chapter 7 code examples

---
+++ src/main/java/com/company/list/ArrayList.java
@@ -0,0 +1,34 @@
+package com.company.list;
+
+public class ArrayList<E> implements List<E> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E get(int i) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public E set(int i, E e) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public void add(int i, E e) {
+
+    }
+
+    @Override
+    public E remove(int i) throws IndexOutOfBoundsException {
+        return null;
+    }
+}
