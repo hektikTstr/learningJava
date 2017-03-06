59aee931efead2d3f6bd63eaa4fee0b469a1d7bc
hektikTstr <shawn.zhang.xmn@gmail.com>
2017-03-06 21:46:20 +0800

    Chapter 7 creativity exercise C-7.26.

--- src/main/java/com/company/list/CircularlyResizableArrayList.java
+++ src/main/java/com/company/list/CircularlyResizableArrayList.java
@@ -4,24 +4,33 @@ import org.testng.annotations.Test;
 
 import java.util.Iterator;
 
-// C-7.25
-public class CircularlyArrayList<E> implements List<E> {
+// C-7.26
+public class CircularlyResizableArrayList<E> implements List<E> {
     private static final int CAPACITY = 8;
     private int size = 0;
     private E[] data;
     private int f = 0;
 
-    public CircularlyArrayList(int capacity) {
+    public CircularlyResizableArrayList(int capacity) {
         data = (E[]) new Object[capacity];
     }
 
-    public CircularlyArrayList() {
+    public CircularlyResizableArrayList() {
         this(CAPACITY);
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    private void resize(int capacity) {
+        E[] temp = (E[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[(f + i) % data.length];
+        }
+        data = temp;
+        f = 0;
     }
 
     @Override
@@ -38,14 +47,14 @@ public class CircularlyArrayList<E> implements List<E> {
     @Override
     public E get(int i) throws IndexOutOfBoundsException {
         checkIndex(i, size);
-        return data[(f + i) % CAPACITY];
+        return data[(f + i) % data.length];
     }
 
     @Override
     public E set(int i, E e) throws IndexOutOfBoundsException {
         checkIndex(i, size);
-        E temp = data[(f + i) % CAPACITY];
-        data[(f + i) % CAPACITY] = e;
+        E temp = data[(f + i) % data.length];
+        data[(f + i) % data.length] = e;
         return temp;
     }
 
@@ -53,32 +62,32 @@ public class CircularlyArrayList<E> implements List<E> {
     public void add(int i, E e) throws IndexOutOfBoundsException {
         checkIndex(i, size + 1);
         if (size == data.length) {
-            throw new IllegalStateException("Array is full");
+            resize(2 * data.length);
         }
         if (i == 0 && size != 0) {
-            f = (f - 1 + CAPACITY) % CAPACITY;
+            f = (f - 1 + data.length) % data.length;
         } else {
             for (int k = size; k > i; k--) {
-                data[(f + k + CAPACITY) % CAPACITY] = data[(f + k + CAPACITY - 1) % CAPACITY];
+                data[(f + k) % data.length] = data[(f + k + data.length - 1) % data.length];
             }
         }
-        data[(f + i) % CAPACITY] = e;
+        data[(f + i) % data.length] = e;
         size++;
     }
 
     @Override
     public E remove(int i) throws IndexOutOfBoundsException {
         checkIndex(i, size);
-        E temp = data[(f + i) % CAPACITY];
+        E temp = data[(f + i) % data.length];
         if (i == 0) {
             int tempIndex = f;
-            f = (f + 1) % CAPACITY;
+            f = (f + 1) % data.length;
             data[tempIndex] = null;
         } else {
             for (int k = i; k < size - 1; k++) {
-                data[(f + k) % CAPACITY] = data[(f + k + 1) % CAPACITY];
+                data[(f + k) % data.length] = data[(f + k + 1) % data.length];
             }
-            data[(f + size - 1) % CAPACITY] = null;
+            data[(f + size - 1) % data.length] = null;
         }
         size--;
         return temp;
@@ -91,13 +100,16 @@ public class CircularlyArrayList<E> implements List<E> {
 
     @Test
     public void test() {
-        List<Integer> list = new CircularlyArrayList<>();
+        List<Integer> list = new CircularlyResizableArrayList<>();
         list.add(0, 1);
         list.add(1, 2);
         list.add(2, 3);
-        list.add(1, 4);
         list.add(0, 0);
         list.add(0, 9);
+        list.add(3, 8);
+        list.add(1, 7);
+        list.add(2, 6);
+        list.add(0, 5);
         list.get(0);
         list.get(1);
         list.get(3);
