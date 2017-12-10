7fdc03e0b76ceafc011dda1df77722891014283d
hektikTstr <shawn.zhang.xmn@gmail.com>
2017-12-10 14:20:57 +0800

    add AdjacencyListGraph implementation.

--- src/main/java/com/company/graph/AdjacencyListGraph.java
+++ src/main/java/com/company/graph/AdjacencyListGraph.java
@@ -1,49 +1,62 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.LinkedList;
-//import java.lang.IllegalArgumentException;
+import java.util.ArrayList;
 
 public class AdjacencyListGraph<V, E> implements Graph<V, E> {
     private class VertexImpl<V> implements Vertex<V> {
         private V element;
-        private Map<Vertex<V>, Edge<E>> outgoing, incoming;
+        private List<Edge<E>> adjacencyOutgoing, adjacencyIncoming;
 
         public VertexImpl(V element, boolean isDirected) {
             this.element = element;
-            outgoing = new HashMap<Vertex<V>, Edge<E>>();
-            incoming = isDirected ? new HashMap<Vertex<V>, Edge<E>>() : outgoing;
+            adjacencyOutgoing = new LinkedList<>();
+            if (isDirected) {
+                adjacencyIncoming = new LinkedList<>();
+            } else {
+                adjacencyIncoming = adjacencyOutgoing;
+            }
         }
+
         public V getElement() {
             return element;
         }
-        public boolean validate(Graph<V,E> graph) {
-            return (AdjacencyMapGraph.this == graph);
+
+        public boolean validate(Graph<V, E> graph) {
+            return (AdjacencyListGraph.this == graph);
         }
-        public Map<Vertex<V>, Edge<E>> getOutgoing() {
-            return outgoing;
+
+        public List<Edge<E>> getOutgoing() {
+            return adjacencyOutgoing;
         }
-        public Map<Vertex<V>, Edge<E>> getIncoming() {
-            return incoming;
-        }        
+
+        public List<Edge<E>> getIncoming() {
+            return adjacencyIncoming;
+        }
     }
 
     private class EdgeImpl<E> implements Edge<E> {
         private E element;
         private Vertex<V>[] endpoints;
-        @SuppressWarnings({"unchecked"})
+
+        @SuppressWarnings({ "unchecked" })
         public EdgeImpl(Vertex<V> u, Vertex<V> v, E elem) {
-            endpoints = new Vertex[]{u, v};
+            endpoints = new Vertex[] { u, v };
             element = elem;
         }
+
         public E getElement() {
             return element;
         }
+
         public Vertex<V>[] getEndpoints() {
             return endpoints;
         }
+
         public boolean validate(Graph<V, E> graph) {
-            return (AdjacencyMapGraph.this == graph);
+            return (AdjacencyListGraph.this == graph);
         }
     }
 
@@ -51,60 +64,86 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
     private List<Vertex<V>> vertices = new LinkedList<>();
     private List<Edge<E>> edges = new LinkedList<>();
 
-    public AdjacencyMapGraph(boolean isDirected) {
+    public AdjacencyListGraph(boolean isDirected) {
         this.isDirected = isDirected;
     }
-    @SuppressWarnings({"unchecked"})
+
+    @SuppressWarnings({ "unchecked" })
     private VertexImpl<V> validate(Vertex<V> vertex) throws IllegalArgumentException {
-        if (!(vertex instanceof VertexImpl)) throw new IllegalArgumentException("Invalid vertex");
+        if (!(vertex instanceof VertexImpl))
+            throw new IllegalArgumentException("Invalid vertex");
         VertexImpl<V> vert = (VertexImpl<V>) vertex;
-        if (!vert.validate(this)) throw new IllegalArgumentException("Invalid vertex");
+        if (!vert.validate(this))
+            throw new IllegalArgumentException("Invalid vertex");
         return vert;
     }
-    @SuppressWarnings({"unchecked"})
+
+    @SuppressWarnings({ "unchecked" })
     private EdgeImpl<E> validate(Edge<E> e) throws IllegalArgumentException {
-        if (!(e instanceof EdgeImpl)) throw new IllegalArgumentException("Invalid edge");
+        if (!(e instanceof EdgeImpl))
+            throw new IllegalArgumentException("Invalid edge");
         EdgeImpl<E> edge = (EdgeImpl<E>) e;
-        if (!edge.validate(this)) throw new IllegalArgumentException("Invalid edge");
+        if (!edge.validate(this))
+            throw new IllegalArgumentException("Invalid edge");
         return edge;
     }
+
     public int numVertices() {
         return vertices.size();
     }
+
     public int numEdges() {
         return edges.size();
     }
+
     public Iterable<Vertex<V>> vertices() {
         return vertices;
     }
+
     public Iterable<Edge<E>> edges() {
         return edges;
     }
+
+    @SuppressWarnings({ "unchecked" })
     public int outDegree(Vertex<V> v) throws IllegalArgumentException {
         VertexImpl<V> vert = validate(v);
         return vert.getOutgoing().size();
     }
+
+    @SuppressWarnings({ "unchecked" })
     public int inDegree(Vertex<V> v) throws IllegalArgumentException {
         VertexImpl<V> vert = validate(v);
         return vert.getIncoming().size();
     }
+
+    @SuppressWarnings({ "unchecked" })
     public Iterable<Edge<E>> outgoingEdges(Vertex<V> v) throws IllegalArgumentException {
         VertexImpl<V> vert = validate(v);
-        return vert.getOutgoing().values();
+        return vert.getOutgoing();
     }
+
+    @SuppressWarnings({ "unchecked" })
     public Iterable<Edge<E>> incomingEdges(Vertex<V> v) throws IllegalArgumentException {
         VertexImpl<V> vert = validate(v);
-        return vert.getIncoming().values();
+        return vert.getIncoming();
     }
+
+    @SuppressWarnings({ "unchecked" })
     public Edge<E> getEdge(Vertex<V> u, Vertex<V> v) throws IllegalArgumentException {
         VertexImpl<V> vert = validate(u);
-        return vert.getOutgoing().get(v);
+        for (Edge<E> edge : vert.getOutgoing()) {
+            if (endVertices(edge)[1] == v)
+                return edge;
+        }
+        return null;
     }
-    @SuppressWarnings({"unchecked"})
+
+    @SuppressWarnings({ "unchecked" })
     public Vertex<V>[] endVertices(Edge<E> e) throws IllegalArgumentException {
         EdgeImpl<E> edge = (EdgeImpl<E>) e;
         return edge.getEndpoints();
     }
+
     public Vertex<V> opposite(Vertex<V> v, Edge<E> e) throws IllegalArgumentException {
         Vertex<V>[] endV = endVertices(e);
         if (v == endV[0]) {
@@ -115,45 +154,51 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
             throw new IllegalArgumentException("Vertex is not indent to this edge");
         }
     }
-    @SuppressWarnings({"unchecked"})
+
+    @SuppressWarnings({ "unchecked" })
     public Vertex<V> insertVertex(V element) {
         VertexImpl<V> vert = new VertexImpl(element, isDirected);
         vertices.add(vert);
         return vert;
     }
-    @SuppressWarnings({"unchecked"})
+
+    @SuppressWarnings({ "unchecked" })
     public Edge<E> insertEdge(Vertex<V> u, Vertex<V> v, E element) throws IllegalArgumentException {
         if (getEdge(u, v) == null) {
             EdgeImpl<E> edge = new EdgeImpl(u, v, element);
             edges.add(edge);
             VertexImpl<V> startVertex = validate(u);
             VertexImpl<V> endVertex = validate(v);
-            startVertex.getOutgoing().put(v, edge);
-            endVertex.getIncoming().put(u, edge);
+            startVertex.getOutgoing().add(edge);
+            endVertex.getIncoming().add(edge);
             return edge;
         } else {
             throw new IllegalArgumentException("Edge from u to v already exist");
         }
     }
+
     public void removeVertex(Vertex<V> v) throws IllegalArgumentException {
-        VertexImpl<V> vert= validate(v);
-        for (Edge<E> e : vert.getOutgoing().values())
+        VertexImpl<V> vert = validate(v);
+        for (Edge<E> e : vert.getOutgoing()) {
             removeEdge(e);
-        for (Edge<E> e : vert.getIncoming().values())
+        }
+        for (Edge<E> e : vert.getIncoming()) {
             removeEdge(e);
+        }
         vertices.remove(vert);
     }
-    @SuppressWarnings({"unchecked"})
+
+    @SuppressWarnings({ "unchecked" })
     public void removeEdge(Edge<E> e) throws IllegalArgumentException {
         EdgeImpl<E> edge = validate(e);
         edges.remove(edge);
-        VertexImpl<V>[] endpoints = (VertexImpl[]) edge.getEndpoints();
-        endpoints[0].getOutgoing().remove(endpoints[1]);
-        endpoints[1].getIncoming().remove(endpoints[0]);
+        Vertex<V>[] vertices = endVertices(edge);
+        ((VertexImpl<V>) vertices[0]).getOutgoing().remove(edge);
+        ((VertexImpl<V>) vertices[1]).getIncoming().remove(edge);
     }
 
     public static void main(String[] args) {
-        Graph<String, Integer> undirectGraph = new AdjacencyMapGraph<>(false);
+        Graph<String, Integer> undirectGraph = new AdjacencyListGraph<>(false);
         Vertex<String> vA = undirectGraph.insertVertex("A");
         Vertex<String> vB = undirectGraph.insertVertex("B");
         Vertex<String> vC = undirectGraph.insertVertex("C");
@@ -176,6 +221,6 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
         }
         System.out.println("Vertex num = " + undirectGraph.numVertices());
         System.out.println("Edge num = " + undirectGraph.numEdges());
-        Graph<String, Integer> directGraph = new AdjacencyMapGraph<>(true);
+        Graph<String, Integer> directGraph = new AdjacencyListGraph<>(true);
     }
 }
